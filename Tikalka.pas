4f4fafa2492d8f8a8b2fbb9d9dcc5a5e25ea32ca
pavtiger <36619129+pavtiger@users.noreply.github.com>
2019-02-02 12:06:31 +0300

    This is a template for Pascal buttons

---
+++ Tikalka.pas
@@ -0,0 +1,14 @@
+﻿uses GraphABC;
+
+procedure MouseDown(mousex, mousey, mb: integer);
+begin
+end;
+  
+procedure KeyDown(key: integer);
+begin
+end;
+
+begin
+  OnMouseDown := MouseDown;
+  OnKeyDown := KeyDown;
+end.
